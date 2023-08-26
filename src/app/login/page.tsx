bc80947772c01861adccd647a13802c2a72ff015
MatheusRelvinhas <matheusrelvinhas@gmail.com>
2023-08-26 03:44:47 -0300

    login-page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,23 +2,26 @@
 
 import LoginForm from '@/components/LoginForm/LoginForm';
 import React from 'react';
-import { useRouter } from 'next/navigation';
 import { useGlobalContext } from '@/Context/store';
+import AddItemForm from '@/components/AddItemForm/AddItemForm';
 
 const LoginPage: React.FC = () => {
   const { isLogin } = useGlobalContext();
   
-  const router = useRouter();
-
-  if (isLogin) {
-    router.push('/partner');
-    return null; // Retornar nulo para interromper a renderização atual
-  }
 
   return (
     <div>
-      <h1>Login</h1>
-      <LoginForm />
+      {isLogin ? (
+        <>
+          <h1>Portal Parceiro</h1>
+          <AddItemForm />
+        </>
+      ) : (
+        <>
+          <h1>Você não está logado. Por favor, faça login para acessar.</h1>
+          <LoginForm />
+        </>
+      )}
     </div>
   );
 };
