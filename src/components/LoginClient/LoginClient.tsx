8eb01ce56b821392ada14f95dc5aee97ca507999
MatheusRelvinhas <matheusrelvinhas@gmail.com>
2023-09-30 16:07:17 -0300

    use-client-error

--- src/components/LoginClient/LoginClient.tsx
+++ src/components/LoginClient/LoginClient.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import { useGlobalContext } from '@/Context/store';
 import AddClientForm from '../AddClientForm/AddClientForm';
--- src/components/LoginPurchaseRequest/LoginPurchaseRequest.tsx
+++ src/components/LoginPurchaseRequest/LoginPurchaseRequest.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useRef, RefObject } from 'react';
 import { useGlobalContext } from '@/Context/store';
 import './LoginPurchaseRequest.css';
