60c1222347f0165eb7dcbab6e4a4cadb05ca4ae5
MatheusRelvinhas <matheusrelvinhas@gmail.com>
2023-10-16 16:03:49 -0300

    error........................

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,10 @@
 'use client';
 
-import LoginForm from '@/components/LoginForm/LoginForm';
 import React from 'react';
 import { useGlobalContext } from '@/Context/store';
-import LoginNavigation from '@/components/LoginNavigation/LoginNavigation';
-import Loading from '@/components/Loading/Loading';
-import ErrorMessage from '@/components/ErrorMessage/ErrorMessage';
 
 const LoginPage: React.FC = () => {
-  //const { isLogin, isLoading, alertLogin, errorMessage } = useGlobalContext();
+  const {  } = useGlobalContext();
   
   return (
     <div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
-import Header from '@/components/Header/Header';
-import Main from '@/components/Main/Main';
+
 
 export default function Home() {
   return (
