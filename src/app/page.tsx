23f951fa2de03d5209fcf96a17f636345ca34ae2
MatheusRelvinhas <matheusrelvinhas@gmail.com>
2023-10-16 17:03:38 -0300

    error-5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Header from '@/components/Header/Header';
+import Main from '@/components/Main/Main';
+
 export default function Home() {
   return (
     <>
-      {/*<Header />
-      {/* <Main /> */}
-      TESTE
+      <Header />
+      <Main />
     </>
   );
 }
